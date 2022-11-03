b8ef85ab598a4346fd0fe3ed0582e2681aa55d0f
Andrey Maksimov <andrey@tenderowl.com>
2022-11-03 20:54:21 +0300

    Change to async read

--- lib/csv_reading.dart
+++ lib/csv_reading.dart
@@ -11,3 +11,16 @@ List<String>? readCsvSync(String path) {
   // Read lines sync and return them.
   return File(path).readAsLinesSync();
 }
+
+// Wrap response in a `Future` object and mark function as `async`
+Future<List<String>?> readCsv(String path) async {
+  // Create `File` object
+  final file = File(path);
+  // Check if file is ready to be read :)
+  if (!await file.exists()) {
+    return null;
+  }
+
+  // Read lines sync and return them.
+  return await File(path).readAsLines();
+}
